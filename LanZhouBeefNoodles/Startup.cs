8f27a6ce6fb2246ca5680cfe6bf2a0d3adfe9186
rickeygong <salabook@qq.com>
2022-08-29 23:45:12 +0800

    配置路由

--- LanZhouBeefNoodles/Startup.cs
+++ LanZhouBeefNoodles/Startup.cs
@@ -16,6 +16,7 @@ namespace LanZhouBeefNoodles
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();//注入内置的组件
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,21 @@ namespace LanZhouBeefNoodles
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();//启用路由（Conventional routing）
+                endpoints.MapControllers();//启用路由（Attribute routing）
+
+                //endpoints.MapGet("/a", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World! I'm A!");
+                //});
+                //endpoints.MapGet("/b", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World! I'm B!");
+                //});
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
         }
     }
---
+++ LanZhouBeefNoodles/controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanZhouBeefNoodles.controllers
+{
+    //[Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+
+        public string Index()
+        {
+            return "Hello from home";
+        }
+
+        public string About()
+        {
+            return "Hello from about";
+        }
+    }
+}
